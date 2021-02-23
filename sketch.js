01e6d9c82a1b311a0f183e13624d772de9ecc91a
aith <ariiramanesh@gmail.com>
2021-02-22 23:48:50 -0800

    changed direction orientation

--- sketch.js
+++ sketch.js
@@ -1,20 +1,30 @@
+// http://algorithmicbotany.org/
 let can; let canw = 800; let canh = 800;
 
 
 let rules = {
-    0: ["1[0]0"],
+    0: ["1[0[0]]"],
     1: ["11"]
 }
+
+let angle;
+let axiom = "0"
+
 function setup() {
     can = createCanvas(canh, canw)
-    frameRate(60)
-    let axiom = "0"
+    frameRate(20)
     axiom = doRules(axiom)
     print(axiom)
+    axiom = doRules(axiom)
+    axiom = doRules(axiom)
+    axiom = doRules(axiom)
+    axiom = doRules(axiom)
+    axiom = doRules(axiom)
     axiom = doRules(axiom)
     axiom = doRules(axiom)
     turtle(axiom)
     print(axiom)
+    slider = createSlider(0, TWO_PI, PI/4, 0.01)
 }
 
 function doRules(axiom) {
@@ -32,11 +42,9 @@ function doRules(axiom) {
     return postman
 }
 
-    let len = 50
+let len = 1
 function turtle(axiom) {
-    len *= 0.5;
-    let angle = radians(45)
-    stroke(000)
+    // stroke(000)
     translate(canw/2, canh/2)
     for(let i = 0; i < axiom.length; i++) {
         let c = axiom.charAt(i)
@@ -45,6 +53,7 @@ function turtle(axiom) {
                 line(0,0,0,-len)
                 break;
             case "1":
+                // line(0,0,0, -len/4)
                 line(0,0,0, -len)
                 translate(0,-len)
                 break;
@@ -62,5 +71,11 @@ function turtle(axiom) {
 
 let start = "F"
 function draw() {
-    
+    background(111111)
+    angle = slider.value()
+    turtle(axiom)
+    // background("white")
+    // turtle(axiom)
+    // angle =(sin(frameCount/10)) * 2*Math.PI
+    // print(angle)
 }
